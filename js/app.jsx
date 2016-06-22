8bde74304281db43604316f627afe4837378c5d5
Marcus <mhellams@hotmail.com>
2016-06-21 23:33:40 -0400

    Passing data to components

--- js/app.jsx
+++ js/app.jsx
@@ -1,7 +1,54 @@
+
 var App = React.createClass({
 	render: function(){
-		return (React.createElement('div', null, 'Welcome to Adequate, Mike'));
+
+		console.log(this.props);
+
+		
+		var headings = this.props.headings.map(function(heading){
+			return <th>{heading}</th>
+		});
+
+		var rows = this.props.data.map(function(row){
+			return <tr>
+				<td>{row.when}</td>
+				<td>{row.who}</td>
+				<td>{row.description}</td>
+			</tr>
+		});
+		return  <div>
+		<h1>{this.props.title}</h1>
+		
+				<table className="table table-bordered">
+							<thead>
+								<tr>
+									{headings}
+								</tr>
+							</thead>
+							<tbody>
+								{rows}
+							</tbody>
+					</table>
+					</div>
 	}
 });
 
-ReactDOM.render(React.createElement(App), document.getElementById('app'));
+var head = ['Last change at', 'By Author', 'Summary'];
+var dat = [{ "when": "2 minutes ago",
+				"who": "Jill Dupre",
+				"description": "Created new account"
+				},
+				{
+				"when": "1 hour ago",
+				"who": "Lose White",
+				"description": "Added fist chapter"
+				},
+				{
+				"when": "2 hours ago",
+				"who": "Jordan Whash",
+				"description": "Created new account"
+				}];
+
+var title = 'Recent Changes';
+
+ReactDOM.render(<App data={dat} headings={head} title={title} />, document.getElementById('app'));
